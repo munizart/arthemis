f1474b2a9791b92a482229798df9dd571e8117bf
Gabriel Luis <gabriel.luis.cinza@gmail.com>
2018-10-02 13:27:47 -0300

    change default orientation to two spaces

--- Container.js
+++ Container.js
@@ -15,39 +15,39 @@ const T_FACTORY = Symbol('factory')
  * @return {Container~Descriptor} A descriptor representing a dependency
  */
 function depDescriptor (name, Definition, deps, type) {
-    return {
-        name,
-        Definition,
-        deps,
-        type
-    }
+  return {
+    name,
+    Definition,
+    deps,
+    type
+  }
 }
 
 function getSingletonInstance (descriptor, dependencies) {
-    if (!descriptor.instance) {
-        descriptor.instance = new descriptor.Definition(...dependencies)
-    }
-    return descriptor.instance
+  if (!descriptor.instance) {
+    descriptor.instance = new descriptor.Definition(...dependencies)
+  }
+  return descriptor.instance
 }
 
 /**
  * @constructs Container
  */
 function containerFactory () {
-    const storage = new Map()
+  const storage = new Map()
 
-    return {
-        /**
+  return {
+    /**
          * Register a class that could have more than one instance
          * @memberof Container#
          * @param  {String}    name         Name for the dependency
          * @param  {Function}  definition   Class constructor
          * @param  {...String} deps         dependencies to resolve into constructor call
          */
-        class (name, definition, ...deps) {
-            storage.set(name, depDescriptor(name, definition, deps, T_CLASS))
-        },
-        /**
+    class (name, definition, ...deps) {
+      storage.set(name, depDescriptor(name, definition, deps, T_CLASS))
+    },
+    /**
          * Register a class that should have at most one instance
          *
          * @memberof Container#
@@ -55,10 +55,10 @@ function containerFactory () {
          * @param  {Function}  definition   Class constructor
          * @param  {...String} deps         dependencies to resolve into constructor call
          */
-        singleton (name, definition, ...deps) {
-            storage.set(name, depDescriptor(name, definition, deps, T_SINGLETON))
-        },
-        /**
+    singleton (name, definition, ...deps) {
+      storage.set(name, depDescriptor(name, definition, deps, T_SINGLETON))
+    },
+    /**
          * Register a factory
          *
          * @memberof Container#
@@ -66,58 +66,58 @@ function containerFactory () {
          * @param  {Function}  definition   Factory function
          * @param  {...String} deps         dependencies to resolve into factory call
          */
-        factory (name, definition, ...deps) {
-            storage.set(name, depDescriptor(name, definition, deps, T_FACTORY))
-        },
-        /**
+    factory (name, definition, ...deps) {
+      storage.set(name, depDescriptor(name, definition, deps, T_FACTORY))
+    },
+    /**
          * Register a literal value into container
          *
          * @memberof Container#
          * @param  {String}    name        Name for the dependency
          * @param  {*}         definition  The item value
          */
-        literal (name, definition) {
-            storage.set(name, depDescriptor(name, definition, [], T_LITERAL))
-        },
+    literal (name, definition) {
+      storage.set(name, depDescriptor(name, definition, [], T_LITERAL))
+    },
 
-        /**
+    /**
          * Get a resolved dependency
          *
          * @memberof Container#
          * @param  {String} name dependency name
          * @return {Object}
          */
-        get (name) {
-            const descriptor = storage.get(name)
-            if (descriptor) {
-                if (descriptor.type !== T_LITERAL) {
-                    const resolvedDeps = descriptor.deps.map(this.get.bind(this))
-                    switch (descriptor.type) {
-                    case T_FACTORY:
-                        return descriptor.Definition(...resolvedDeps)
-                    case T_SINGLETON:
-                        return getSingletonInstance(descriptor, resolvedDeps)
-                    case T_CLASS:
-                        return new descriptor.Definition(...resolvedDeps)
-                    }
-                } else {
-                    return descriptor.Definition
-                }
-            }
-        },
+    get (name) {
+      const descriptor = storage.get(name)
+      if (descriptor) {
+        if (descriptor.type !== T_LITERAL) {
+          const resolvedDeps = descriptor.deps.map(this.get.bind(this))
+          switch (descriptor.type) {
+            case T_FACTORY:
+              return descriptor.Definition(...resolvedDeps)
+            case T_SINGLETON:
+              return getSingletonInstance(descriptor, resolvedDeps)
+            case T_CLASS:
+              return new descriptor.Definition(...resolvedDeps)
+          }
+        } else {
+          return descriptor.Definition
+        }
+      }
+    },
 
-        /**
+    /**
          * Call the given function within the requested dependencies resolved
          *
          * @memberof Container#
          * @param  {Function}    fn           Function to call within deps
          * @param  {...String[]} dependencies List of dependencies to resolve
          */
-        resolve (fn, ...dependencies) {
-            const resolvedDeps = dependencies.map(this.get.bind(this))
-            fn(...resolvedDeps)
-        }
+    resolve (fn, ...dependencies) {
+      const resolvedDeps = dependencies.map(this.get.bind(this))
+      fn(...resolvedDeps)
     }
+  }
 }
 
 module.exports = containerFactory
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = (grunt) => {
-    require('load-grunt-tasks')(grunt)
+  require('load-grunt-tasks')(grunt)
 
-    grunt.initConfig({
-        jsdoc2md: {
-            doc: {
-                src: ['*.js'],
-                dest: 'docs/README.md'
-            }
-        },
-        eslint: {
-            target: ['*.js']
-        }
-    })
+  grunt.initConfig({
+    jsdoc2md: {
+      doc: {
+        src: ['*.js'],
+        dest: 'docs/README.md'
+      }
+    },
+    eslint: {
+      target: ['*.js']
+    }
+  })
 
-    grunt.registerTask('default', [
-        'eslint',
-        'jsdoc2md'
-    ])
+  grunt.registerTask('default', [
+    'eslint',
+    'jsdoc2md'
+  ])
 }
